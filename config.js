af3445388ea19b6353b2f659ae08ce821c621d03
lakshi4 <148441415+lakshi4@users.noreply.github.com>
2025-08-07 12:24:29 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://Isuru4:Isurulakshan4%23@studentmanagementsystem.5e4dds6.mongodb.net/health_n?retryWrites=true&w=majority&appName=StudentManagementSystem'
